71a1993e3daf85992722d9b6b4238cbb099b8451
Muhammad Fachry Ramadhan <78468976+fachrydan@users.noreply.github.com>
2023-05-14 22:32:01 +0700

    update suites test cases has been added

---
+++ Scripts/Fitur Tips/Tips-Minuman/Script1684076519617.groovy
@@ -0,0 +1,56 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\Fachry Ramadhan\\Katalon Studio\\HealthyMomsTest\\mobileapp\\HealthyMoms_v2.0.apk', 
+    true)
+
+Mobile.setText(findTestObject('Object Repository/TestTipsMinuman/android.widget.EditText - Email'), 'putri@gmail.com', 0)
+
+Mobile.setText(findTestObject('Object Repository/TestTipsMinuman/android.widget.EditText - Password'), 'putri123', 0)
+
+Mobile.tap(findTestObject('Object Repository/TestTipsMinuman/android.widget.Button - LOGIN'), 0)
+
+Mobile.tap(findTestObject('Object Repository/TestTipsMinuman/android.widget.FrameLayout'), 0)
+
+Mobile.tap(findTestObject('Object Repository/TestTipsMinuman/android.widget.FrameLayout (1)'), 0)
+
+Mobile.scrollToText('Jus Wortel')
+
+Mobile.tap(findTestObject('Object Repository/TestTipsMinuman/android.view.ViewGroup'), 0)
+
+Mobile.scrollToText('radikal')
+
+Mobile.pressBack()
+
+Mobile.scrollToText('Air Kelapa')
+
+Mobile.tap(findTestObject('Object Repository/TestTipsMinuman/android.view.ViewGroup (1)'), 0)
+
+Mobile.scrollToText('kram')
+
+Mobile.pressBack()
+
+Mobile.scrollToText('Teh Herbal')
+
+Mobile.tap(findTestObject('Object Repository/TestTipsMinuman/android.view.ViewGroup (2)'), 0)
+
+Mobile.scrollToText('Rooibos')
+
+Mobile.closeApplication()
+
---
+++ Test Suites/Suite Test HealthyMoms.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite Test HealthyMoms</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>bfd6dabd-59df-4f81-a68a-84d4cc5241fd</testSuiteGuid>
+   <testCaseLink>
+      <guid>d2f00da8-cce8-424f-a9a2-dd42381debf1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/FiturHPL</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2be718f0-6f9b-45c0-8070-3c7583968ed3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/FiturHubBidan</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b94d260d-3666-40cb-afa4-195e13d9290a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/FiturInformasi</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f1d83748-a0f8-4a96-a211-f448942af2b0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Fitur Tips/Tips-Minuman</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a0f720c8-5789-4a1e-812d-7000877e4489</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Fitur Tips/Tips-Makanan-IntentImplisit</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dc632650-83da-46c1-affc-960a8576d99d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Fitur Tips/Tips-Olahraga</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
